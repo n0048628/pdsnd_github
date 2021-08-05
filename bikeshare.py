bc6571fd4d166627cd0ff2697444144b0df85f3b
Kathleen Durkin <nikrud1001@gmail.com>
2021-08-04 22:36:48 -0400

    improve get_filters documentation

--- bikeshare.py
+++ bikeshare.py
@@ -14,6 +14,7 @@ def get_filters():
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    improving get_filters documentation
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). 
